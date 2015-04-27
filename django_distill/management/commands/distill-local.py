2a8d79c1c519f9fe57ac6cc31391a98ac7d1966b
Joe Harris <j@m.pr>
2015-04-27 04:19:42 +0100

    formatting tweaks

--- django_distill/management/commands/distill-local.py
+++ django_distill/management/commands/distill-local.py
@@ -25,15 +25,21 @@ class Command(BaseCommand):
         static_dir = settings.STATIC_ROOT
         static_url = settings.STATIC_URL
         if not output_dir:
-            raise CommandError('Usage: ./manage.py distill-local [directory]')
+            output_dir = getattr(settings, 'DISTILL_DIR', None)
+            if not output_dir:
+                e = 'Usage: ./manage.py distill-local [directory]'
+                raise CommandError(e)
         if collectstatic:
             self.stdout.write('Distill is running collectstatic...')
             run_collectstatic()
             self.stdout.write('')
             self.stdout.write('collectstatic complete, running distill...')
-            self.stdout.write('')
         output_dir = os.path.abspath(os.path.expanduser(output_dir))
         static_output_dir = os.path.join(output_dir, static_url[1:])
+        self.stdout.write('')
+        self.stdout.write('You have requested to create a static version of')
+        self.stdout.write('this site into the output path directory:')
+        self.stdout.write('')
         self.stdout.write('    Source static path:  {}'.format(static_dir))
         self.stdout.write('    Distill output path: {}'.format(output_dir))
         self.stdout.write('')
@@ -57,20 +63,24 @@ class Command(BaseCommand):
                 raise CommandError('Aborting...')
         self.stdout.write('Loading site URLs')
         self.load_urls()
+        self.stdout.write('')
         self.stdout.write('Rendering site...')
         renderer = DistillRender(output_dir, urls_to_distill)
         for page_uri, http_response in renderer.render():
             full_path = os.path.join(output_dir, page_uri[1:])
             content = http_response.content
-            self.stdout.write('Rendering page: {} -> {} ["{}", {} bytes]'.format(
-                page_uri, full_path, http_response.get('Content-Type'),
-                len(content)))
+            self.stdout.write('Rendering page: {} -> {} ["{}", {} bytes]' \
+                .format(page_uri, full_path, http_response.get('Content-Type'),
+                        len(content)))
             with open(full_path, 'w') as f:
                 f.write(content)
+        self.stdout.write('')
         self.stdout.write('Copying static media...')
-        for file_from, file_to in renderer.copy_static(static_dir, static_output_dir):
+        for file_from, file_to in renderer.copy_static(static_dir,
+            static_output_dir):
             self.stdout.write('Copying static: {} -> {}'.format(file_from,
                 file_to))
+        self.stdout.write('')
         self.stdout.write('Done.')
 
     def load_urls(self):

f33c0ac902eee395bbad11974e61fbb7f2b99728
meeb <meeb@meeb.org>
2018-10-19 23:28:47 +1000

    fix backwards compatability with Django 1.x

--- django_distill/__init__.py
+++ django_distill/__init__.py
@@ -1,10 +1,32 @@
 # -*- coding: utf-8 -*-
 
 
-from django_distill.distill import distill_url, distill_path, distill_re_path
+__version__ = 1.5
 
 
-__version__ = 1.4
+from django import __version__ as django_version
+
+
+from django_distill.distill import distill_url
+from django_distill.errors import DistillError
+
+
+try:
+    from django_distill.distill import distill_re_path
+except ImportError:
+    def distill_re_path(*args, **kwargs):
+        err = ('Your installed version of Django ({}) does not supprt '
+            'django.urls.re_path, please upgrade')
+        raise DistillError(err.format(django_version))
+
+
+try:
+    from django_distill.distill import distill_path
+except ImportError:
+    def distill_path(*args, **kwargs):
+        err = ('Your installed version of Django ({}) does not supprt '
+            'django.urls.path, please upgrade')
+        raise DistillError(err.format(django_version))
 
 
 # eof

ea6132b6c21c73265c5b80389c9fb448c9eb7297
meeb <meeb@meeb.org>
2020-10-24 13:27:01 +1100

    remove errant debug print

--- run-tests.py
+++ run-tests.py
@@ -11,7 +11,6 @@ from django.test.utils import get_runner
 if __name__ == '__main__':
     os.environ['DJANGO_SETTINGS_MODULE'] = 'tests.settings'
     django.setup()
-    print(settings.MIDDLEWARE)
     TestRunner = get_runner(settings)
     test_runner = TestRunner(verbosity=2)
     failures = test_runner.run_tests(['tests'])

a2de6376db8fb801c6e8b2f2a07e03575fa59f31
meeb <meeb@meeb.org>
2020-11-10 19:38:46 +1100

    drop python3.3 and python3.4 tags

--- setup.py
+++ setup.py
@@ -35,8 +35,6 @@ setup(
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Topic :: Internet :: WWW/HTTP',

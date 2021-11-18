b8966900931442b3a318b7fe8a6e3deced2e07a3
meeb <meeb@meeb.org>
2021-11-19 02:56:20 +1100

    only check STATIC_ROOT exists when not using --exclude-staticfiles, resolves #52

--- django_distill/management/commands/distill-local.py
+++ django_distill/management/commands/distill-local.py
@@ -42,7 +42,7 @@ class Command(BaseCommand):
                 raise CommandError(e)
         if collectstatic:
             run_collectstatic(stdout)
-        if not os.path.isdir(settings.STATIC_ROOT):
+        if not exclude_staticfiles and not os.path.isdir(settings.STATIC_ROOT):
             e = 'Static source directory does not exist, run collectstatic'
             raise CommandError(e)
         output_dir = os.path.abspath(os.path.expanduser(output_dir))

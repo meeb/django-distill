9264317d11a17ca7a2df8dfbb994ab23b7fbb6be
meeb <meeb@meeb.org>
2021-12-15 18:50:18 +1100

    bump to 2.9.2

--- django_distill/__init__.py
+++ django_distill/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '2.9.1'
+__version__ = '2.9.2'
 
 
 from django import __version__ as django_version
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import sys
 from setuptools import setup, find_packages
 
 
-version = '2.9.1'
+version = '2.9.2'
 
 
 with open('README.md', 'rt') as f:

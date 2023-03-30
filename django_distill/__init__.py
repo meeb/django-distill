a9a48675e06b0d593c4d2214d3301c27563534e5
meeb <meeb@meeb.org>
2023-03-30 18:26:43 +1100

    bump to 3.1.1

--- django_distill/__init__.py
+++ django_distill/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '3.1.0'
+__version__ = '3.1.1'
 
 
 from django import __version__ as django_version
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import sys
 from setuptools import setup, find_packages
 
 
-version = '3.1.0'
+version = '3.1.1'
 
 
 with open('README.md', 'rt') as f:
